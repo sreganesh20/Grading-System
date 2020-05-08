3afa2e7d322b869fdced6a277c2fa88c5b76b4b0
sreganesh20 <65004166+sreganesh20@users.noreply.github.com>
2020-05-08 10:53:35 +0530

    garde.c

---
+++ 1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>//85 100  70 84   55 69   40 54   <40
+int main()
+{
+    int marks;
+    char grade;
+    printf("Enter the marks by the student: ");
+    scanf("%d",&marks);
+    if(marks>=85 && marks<=100)
+    {
+        grade='A';
+    }
+    else if(marks>=70 && marks<85)
+    {
+        grade='B';
+    }
+    else if(marks>=55 && marks<70)
+    {
+        grade='C';
+    }
+    else if(marks>=40 && marks<55)
+    {
+        grade='D';
+    }
+    else if(marks<40)
+    {
+        grade='F';
+    }
+    else
+    {
+        printf("\nEntered marks are wrong!");
+    }
+    printf("\nThe grade %c is given for %d marks",grade,marks);
+    return 0;
+}
